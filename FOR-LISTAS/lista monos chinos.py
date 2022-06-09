02e8c619558a4068bfedcfd090f217d2af4c6789
strpalgato <sebabored@gmail.com>
2022-06-08 20:55:04 -0400

    Updated

--- FOR-LISTAS/lista monos chinos.py
+++ FOR-LISTAS/lista monos chinos.py
@@ -1,8 +1,28 @@
+from os import system
+system("cls")
+
 listaA=["Berserk", "Death Note", "Dr.Stone", "Dragon ball Z", "Steins Gate"]
 listaG=[]
+
 for i in listaA:
-    print("Mono chino: {}".format(i))
-    Genero=input("Ingrese la demografia del mono chino de la lista: ")
-    listaG.append(Genero)
+    while(True):
+        print("Mono chino: {}".format(i))
+        print("")
+        try:
+            Genero=input("Ingrese la demografia del mono chino de la lista: ")
+            if(Genero=="Seinen") or (Genero=="Shonen"):
+                listaG.append(Genero)
+                break
+            else:
+                print("")
+                print("Error, debe ingresar una demografia valida (Shonen/Seinen)")
+                continue
+        except ValueError:
+            print("")
+            print("Error, debe ingresar una demografia valida (Shonen/Seinen)")
+            continue
+
+print("")
 for i in range(len(listaA)):
     print("La demografia del mono chino " + listaA[i] + " corresponde a " + listaG[i])
+print("")
